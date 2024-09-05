6040ff224a75be255b1d1a45dfc75d509875ab6d
Saksham Singh Rathore <sakshamsinghrathore1304@gmail.com>
2024-09-06 00:19:08 +0530

    PPL Lab 6

--- PPL_Lab6/assignment_06_question_03.cpp
+++ PPL_Lab6/assignment_06_question_03.cpp
@@ -1,11 +1,11 @@
 /*
 Assignment 06
-Question 3: Calculating simple interest by passing principle ,rate,time values into a function S_Interest which contain an r values as default 0.075 and printing simple intrest with default value and with given value
+Question 3: Calculating simple interest by passing principle ,rate,time values into a function S_Interest which contain an r values as default 0.075 and printing simple interest with default value and with given value
 */
 
 #include <bits/stdc++.h>
 using namespace std;
-double S_Intrest(double principle, double time, double rate = 0.075)
+double S_Interest(double principle, double time, double rate = 0.075)
 {
     double ans = (principle * rate * time) / 100;
     return ans;
@@ -18,17 +18,17 @@ int main()
     cout << "Enter time: ";
     double time;
     cin >> time;
-    cout << "Do you wish to use default intrest rate ? Enter 0 for false and 1 for true " << endl;
+    cout << "Do you wish to use default interest rate ? Enter 0 for false and 1 for true " << endl;
     bool input;
     cin >> input;
     if (input)
     {
-        cout << "The Simple Intrest is " << S_Intrest(principle, time) << endl;
+        cout << "The Simple Interest is " << S_Interest(principle, time) << endl;
         return 0;
     }
     cout << "Enter rate: ";
     double rate;
     cin >> rate;
-    cout << "The Simple Intrest is " << S_Intrest(principle, time, rate) << endl;
+    cout << "The Simple Interest is " << S_Interest(principle, time, rate) << endl;
     return 0;
 }
