febb7b5a2fb61410dbdf1c6fe8088c5f9b47886c
Saksham Singh Rathore <sakshamsinghrathore1304@gmail.com>
2024-10-18 02:44:58 +0530

    PPL Lab 5

---
+++ PPL_Lab_05/assignment_05_question_03.cpp
@@ -0,0 +1,37 @@
+/*
+Assignemnt 05:
+Write to find the power of the given base and exponent by using function pow(base,exponent)
+*/
+
+#include <iostream>
+using namespace std;
+
+int pow(int base, int exponent)
+{
+    int result = 1;
+
+    for (int i = 0; i < exponent; i++)
+    {
+        result *= base;
+    }
+
+    return result;
+}
+
+int main()
+{
+    int base, exponent;
+
+    cout << "Enter the base: ";
+    cin >> base;
+
+    cout << "Enter the exponent: ";
+    cin >> exponent;
+
+    int power = pow(base, exponent);
+
+    // Print the result
+    cout << base << "^" << exponent << " = " << power << endl;
+
+    return 0;
+}
