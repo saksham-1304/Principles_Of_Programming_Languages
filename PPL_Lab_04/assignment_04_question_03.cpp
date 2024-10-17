6dcd22d8e030e9d131918c7b108f76be5e7fd867
Saksham Singh Rathore <sakshamsinghrathore1304@gmail.com>
2024-10-18 02:44:48 +0530

    PPL Lab 4

---
+++ PPL_Lab_04/assignment_04_question_03.cpp
@@ -0,0 +1,35 @@
+/*Assignment 04
+Question 03: Program to reverse the characters in each word of a string
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+void reverseString(string &s, int start, int end)
+{
+    while (start <= end)
+    {
+        swap(s[start++], s[end--]);
+    }
+}
+int main()
+{
+    cout << "Enter the sentence:" << endl;
+    string s;
+    getline(cin, s);
+    cout << "The orignal sentence is: " << s << endl;
+    int count = 0;
+    int i = 0;
+    int start = 0;
+    while (i <= s.size())
+    {
+        if (s[i] == ' ' || s[i] == '\0')
+        {
+            int end = i - 1;
+            reverseString(s, start, end);
+            start = i + 1;
+        }
+        i++;
+    }
+
+    cout << "The reversed sentence is: " << s << endl;
+}
